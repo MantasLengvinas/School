40533c917aadbad00bb75b479e28a7c42946f71c
Mantas <jmlmantas@gmail.com>
2020-01-28 18:15:39 +0200

    Pirkeju srautai done

--- Grade 12/C++/Egzaminai/Pasirengimas/Pirkeju srautai/main.cpp
+++ Grade 12/C++/Egzaminai/Pasirengimas/Pirkeju srautai/main.cpp
@@ -7,13 +7,15 @@
 using namespace std;
 
 const char FD[] = "srautai.data";
-const char FR[] = "srautai.res";
+const char FR[] = "srautai.out";
 
 const int MaxP = 100;
 const int Max = 24;
 
 void Duomenys(int P[], int &n);
 void Srautai(int n, int P[], int PrH[], int PrM[], int PbH[], int PbM[], int kiekP[]);
+void Pusvalandziai(int PrH[], int PrM[], int PbH[], int PbM[], int kiekP[]);
+void Isvedimas(int PrH[], int PrM[], int PbH[], int PbM[], int kiekP[]);
 
 int main(){
 
@@ -22,8 +24,9 @@ int main(){
     int n;
 
     Duomenys(P, n);
+    Pusvalandziai(PrH, PrM, PbH, PbM, kiekP);
     Srautai(n, P, PrH, PrM, PbH, PbM, kiekP);
-
+    Isvedimas(PrH, PrM, PbH, PbM, kiekP);
 
     return 0;
 }
@@ -48,13 +51,48 @@ void Duomenys(int P[], int &n){
 void Pusvalandziai(int PrH[], int PrM[], int PbH[], int PbM[], int kiekP[]){
 
     for(int i = 0; i < 24; i++){
+        kiekP[i] = 0;
+    }
+    for(int i = 0; i < 24; i += 2){
+        PrM[i] = 0;
+        PbM[i] = 30;
+        PrM[i + 1] = 30;
+        PbM[i + 1] = 0;
+    }
+    int val = 8;
+    for(int i = 0; i < 24; i += 2){
+        PrH[i] = val;
+        PbH[i] = val;
+        PrH[i + 1] = val;
+        PbH[i + 1] = val + 1;
+        val++;
+    }
+}
 
+void Srautai(int n, int P[], int PrH[], int PrM[], int PbH[], int PbM[], int kiekP[]){
+
+    int pr, pb;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 24; j++){
+            pr = PrH[j] * 60 + PrM[j];
+            pb = PbH[j] * 60 + PbM[j];
+            if(P[i] > pr && P[i] < pb){
+                kiekP[j]++;
+            }
+        }
     }
 
 }
 
-void Srautai(int n, int P[], int PrH[], int PrM[], int PbH[], int PbM[], int kiekP[]){
-    int P0 = 480, P1 = 1200;
+void Isvedimas(int PrH[], int PrM[], int PbH[], int PbM[], int kiekP[]){
 
-    for(int i = 0; i < )
+    ofstream fr(FR);
+
+    for(int i = 0; i < 24; i++){
+        fr<<PrH[i]<<" "<<PrM[i]<<" "<<PbH[i]<<" "<<PbM[i]<<" "<<kiekP[i]<<endl;
+    }
+
+    fr.close();
+
 }
