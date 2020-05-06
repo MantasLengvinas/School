0e3a75a2be9e3839abc9555ca62a7b991279c7a4
Mantas <jmlmantas@gmail.com>
2020-05-06 18:02:25 +0300

    Pasiruosimas/Darbeliai done

---
+++ Grade 12/C++/Egzaminai/Pasirengimas/Darbeliai/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const char FD[] = "data.txt";
+const char FR[] = "res.txt";
+
+struct _m {
+	string name;
+	vector<int> S;
+};
+
+void getData(int &n, int &k, vector<_m> &M);
+void CalculateCards(vector<_m> &M, vector<_m> &K);
+void OutputResults(vector<_m> K);
+
+int main(){
+
+	int n, k;
+	vector<_m> M;
+	vector<_m> K;
+
+	getData(n, k, M);
+	CalculateCards(M, K);
+	OutputResults(K);
+
+	return 0;
+}
+
+void getData(int &n, int &k, vector<_m> &M){
+
+	ifstream fd(FD);
+	fd >> n >> k;
+	int s;
+	char a;
+	string name;
+	vector<int> S;
+
+	for(int i = 0; i < n; i++){
+        for(int i = 0; i < 15; i++){
+            fd.get(a);
+            name += a;
+        }
+		for(int j = 0; j < k; j++){
+			fd >> s;
+			S.push_back(s);
+		}
+		M.push_back({name, S});
+        S.clear();
+        name.clear();
+	}
+
+	fd.close();
+
+}
+
+void CalculateCards(vector<_m> &M, vector<_m> &K){
+
+	vector<int> S;
+	S.assign(10, 0); //Vektoriaus uzpildymas 0
+
+	for(int i = 0; i < M.size(); i++){
+		for(int j = 0; j < M[i].S.size(); j++){
+			int k = (M[i].S[j]) - 1;
+			S[k]++;
+		}
+		K.push_back({M[i].name, S});
+		S.assign(10, 0);
+	}
+
+}
+
+void OutputResults(vector<_m> K){
+	ofstream fr(FR);
+
+	for(int i = 0; i < K.size(); i++){
+		fr << K[i].name << " ";
+		for(int j = 0; j < K[i].S.size(); j++){
+			fr << K[i].S[j] << " ";
+		}
+	}
+
+	fr.close();
+}
